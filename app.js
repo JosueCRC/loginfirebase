dea4344530c30e06e8ae82a503e0226f06f724c2
Josue Gonzalez <39922816+JosueCRC@users.noreply.github.com>
2020-05-28 15:49:41 -0600

    primer commit
    primer commit

---
+++ app.js
@@ -0,0 +1,106 @@
+function registrar(){
+    
+    var email = document.getElementById('email').value;
+    var contraseña = document.getElementById('contraseña').value;
+    console.log(email);
+    console.log(contraseña);
+
+    firebase.auth().createUserWithEmailAndPassword(email, contraseña)
+    .then(function(){
+        verificar()
+    })
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+   
+    
+}
+
+function ingreso(){
+    
+    var email2 = document.getElementById('email2').value;
+    var contraseña2 = document.getElementById('contraseña2').value;
+    console.log(email2);
+    console.log(contraseña2);
+    firebase.auth().signInWithEmailAndPassword(email2, contraseña2).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+   
+}
+
+function observador(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          console.log('Existe usuario Activo')
+          aparece(user);
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+            console.log('*********')
+            console.log(user.emailVerified);
+            console.log('*********')
+
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          console.log('*********')
+          console.log(displayName);
+          console.log('*********')
+          var providerData = user.providerData;
+          // ...
+        } else {
+          // User is signed out.
+          console.log('No se ha iniciado sesion.')
+          // ...
+        }
+      });
+}
+
+observador();
+
+function aparece(user){
+    var user = user;
+    var contenido = document.getElementById('contenido');
+    if (user.emailVerified){
+        contenido.innerHTML= `
+    <p>Bienvenido!</p>
+    <button onclick="cerrar()">Cerrar sesion</button>
+    `;
+    }
+    
+}
+
+function cerrar(){
+    firebase.auth().signOut()
+    .then(function(){
+
+        console.log('Cerrando Sesion...')
+    })
+    .catch(function(error){
+        console.log(error)
+
+    })
+}
+
+function verificar(){
+    var user = firebase.auth().currentUser;
+
+    user.sendEmailVerification().then(function() {
+    // Email sent.
+    console.log('Enviando Correo...');
+    }).catch(function(error) {
+    // An error happened.
+    console.log(error);
+    });
+}
